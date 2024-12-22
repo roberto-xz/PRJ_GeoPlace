77d099cac36e333676d96744f9c39d9ff1e15d89
roberto-xz <devv-robert@outlook.com>
2024-12-21 21:34:15 -0300

    add: valid page router

--- geoplace_spa/src/router/routers.js
+++ geoplace_spa/src/router/routers.js
@@ -8,7 +8,8 @@ const routers = createRouter({
   routes: [
     {path: '/indexPage',name:'index',component: ()=> import('../pages/indexPage.vue')},
     {path: '/loginPage',name:'login',component: ()=> import('../pages/loginPage.vue')},
-    {path: '/siginPage',name:'sigin',component: ()=> import('../pages/siginPage.vue')}
+    {path: '/siginPage',name:'sigin',component: ()=> import('../pages/siginPage.vue')},
+    {path: '/validPage',name:'valid',component: ()=> import('../pages/validPage.vue')}
   ],
   
 });
