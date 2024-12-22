aaf151204ea0b9a8f0d0dd1734fb435425a38ef2
roberto-xz <devv-robert@outlook.com>
2024-12-22 00:27:15 -0300

    add: right menu component, but , code not  implemented

---
+++ geoplace_spa/src/pages/component_s/app_rmenu.vue
@@ -0,0 +1,25 @@
+
+<script setup>
+</script>
+
+<template>
+    <div id="r-menu-box">
+        <div id="r-menu">ola, mundo</div>
+    </div>    
+</template>
+
+<style scoped>
+    #r-menu-box {
+        position: fixed;
+        top: 0; left: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(0,0,0,.5);
+    }
+        #r-menu {
+            display: block;
+            width: 90%;
+            background-color: #fff;
+            height: 100%;
+        }
+</style>
--- geoplace_spa/src/pages/geoplace_.vue
+++ geoplace_spa/src/pages/geoplace_.vue
@@ -1,8 +1,21 @@
 
 <script setup>
+    import {ref} from "vue"
     import appHeader from "./component_s/app_header.vue";
+    import appRmenu  from "./component_s/app_rmenu.vue"
+
+    const rmenu_opened = ref(false)
+
+    const toogleMenu = function() {
+        rmenu_opened.value = !rmenu_opened.value;
+    }
+
 </script>
 
-<template>
-    <appHeader/>
-</template>
+<template> <div>
+    <appHeader
+        @click-rmenu = "toogleMenu"
+    />
+    
+    <appRmenu v-if="rmenu_opened"/>
+</div></template>
