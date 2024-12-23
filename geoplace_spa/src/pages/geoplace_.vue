ef99e82573dc45dbdd65cc24acdf5a6fd19325ae
roberto-xz <devv-robert@outlook.com>
2024-12-22 22:53:08 -0300

    add: card components, and simple integrate in to 'feeds' page

--- geoplace_spa/src/pages/geoplace_.vue
+++ geoplace_spa/src/pages/geoplace_.vue
@@ -3,6 +3,7 @@
     import {ref} from "vue"
     import appHeader from "./component_s/app_header.vue";
     import appRmenu  from "./component_s/app_rmenu.vue"
+    import cards from "./component_s/app_cardv1.vue"
 
     const rmenu_opened = ref(true)
 
@@ -16,9 +17,8 @@
 <div>
     <appHeader @open-rmenu = "toogleMenu"/>
     <appRmenu v-if="rmenu_opened" @close-rmenu = "toogleMenu"/>
-    
-    <div id="cards-box">
-
-    </div>
+    <cards
+        @open-card="(x)=> console.log(x)"
+    />
 </div>
 </template>
