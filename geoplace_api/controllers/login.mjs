41a303fbb853f3cb90559dfbc00dd5e4901db4b8
roberto-xz <devv-robert@outlook.com>
2025-01-05 00:57:27 -0300

    rota login refatorada

--- geoplace_api/controllers/login.mjs
+++ geoplace_api/controllers/login.mjs
@@ -1,41 +1,39 @@
 
-import 'dotenv/config'
-import isValidLoginData from '../Utils/loginValidInputs.mjs'
+import valid_login from '../utils/login_valid.mjs'
+import creat_token from '../utils/creat_token.mjs'
+
+import returns  from '../returns/returns.mjs'
 import {models} from '../models/models.mjs'
-import returns from '../returns/returns.mjs';
-import jwt from "jsonwebtoken"
+
 
 export const login = async (req, res)=> {
     let user_mail = req.body.user_mail;
     let user_pass = req.body.user_pass;
 
-    if (isValidLoginData(user_mail,user_pass)) {
+    if (valid_login(user_mail,user_pass)) {
         const user = await models.Users.findOne({
             where: {
                 user_email: user_mail,
                 user_pass:  user_pass
             },
-            attributes: ['id','user_name','user_account_status'],
+            attributes:['id','user_name','user_account_status'],
         });
-        
-        if ( user != null ) {
-            let user_payload = {
+        if (user) return res.json(returns.error_invalid_input());
+        if (user.user_account_status == true ) {
+            let user_data = {
                 type: 'login',
                 uuid: user.id, 
                 name: user.user_name
             }
-            
-            if (user.user_account_status == true ) {
-                let token = jwt.sign(user_payload,process.env.api_jkey,{expiresIn: '1h'});
-                return res.status(200).json( returns.success_with_data(token));
-            }
-
-            return res.status(200).json(returns.error_account_not_actived());
+            let token = creat_token(user_data, '24h');
+            res.json(returns.success_with_data(token));
+            return;
         }
-        
-        return res.status(200).json(returns.error_invalid_input());
+
+        res.json(returns.error_account_not_actived());
+        return
     }
     
-    return res.status(200).json(returns.error_invalid_request());
-
+    res.json(returns.error_invalid_request());
+    return
 }
