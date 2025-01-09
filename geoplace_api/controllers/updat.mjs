3bfa3122692d379b4fdaf64f8356f5d8995e6753
roberto-xz <devv-robert@outlook.com>
2025-01-09 11:49:54 -0300

    refatorado,  ainda há muita melhoria a fazer nessa rota, apesar do código parecer estável, pode haver passos que posso burlar/bugar o fluxo de atualização.

--- geoplace_api/controllers/updat.mjs
+++ geoplace_api/controllers/updat.mjs
@@ -4,12 +4,21 @@ import returns from "../returns/returns.mjs";
 import { valid_updat } from "../utils/valid_updat.mjs";
 import { verif_token } from "../utils/verif_token.mjs";
 
-const updat = async (req,res) => {
-    if (token = verif_token(req.body.user_token)) {
-        const user_data = req.body.user_data;
+//update user function
+export const updat = async (req,res) => {
+    const token = verif_token(req.body.user_token); 
+    if (token) {
+        const user_data  = req.body.user_data;
         const data_check = valid_updat(user_data);
-        if (data_check.has_errors)
-            return returns.error_duplicate_entry(data_check.errors);
+ 
+        if (data_check == null || data_check.hasErros) {
+            let json_res = {}
+            if (data_check != null)
+                data_check.invalids.forEach(element=>{
+                    json_res[element] = user_data[element]
+                });
+            return res.json(returns.error_invalid_input(json_res));
+        }
 
         let user = await models.Users.findOne({
             where: {user_email: token.email},
@@ -17,22 +26,22 @@ const updat = async (req,res) => {
         });
 
         if ( user && user.user_account_status == true) {
-            [counts, _] = await models.Users.update({
-                full_name: user_data.full_name,
-                user_name: user_data.user_name,
-                user_phone: user_data.user_phone,
-                user_cpf: user_data.user_cpf,
-                user_city: user_data.user_city,
-                user_mapCords: user_data.user_cords,
-                where: {id: user.id}
-            });
-            if (counts > 0 ) {
-                res.json(returns.success());
-                return
+            try {
+                let result = await models.Users.update(user_data,{"where":{id: user.id}});
+                if (result)
+                    return res.json(returns.success());
+                return res.json(returns.error_operation_failed());
+
+            }catch(error) {
+                /*
+                let json_return = {
+                    errors: error.parent.code,
+                    fields: error.fields
+                } */
+                return res.json(returns.error_duplicate_entry(error.fields));
             }
-            res.json(returns.error_operation_failed());
-            return
         }
+
         res.json(returns.error_account_not_actived());
         return
     }
