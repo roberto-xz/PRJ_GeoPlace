4e02157e57eb86c46c79f748e3a7790af71ff757
roberto-xz <devv-robert@outlook.com>
2025-01-09 11:48:30 -0300

    refatorado,  o codigo está mais simples e, aparentemente sem bugs

--- geoplace_api/utils/valid_updat.mjs
+++ geoplace_api/utils/valid_updat.mjs
@@ -1,7 +1,7 @@
 
 export const valid_updat = (user_json) => {
-    const patterns = {
-        user_fullname: /^[A-Za-zÀ-ÖØ-öø-ÿ\s]{10,120}$/,
+    const patterns_ = {
+        full_name: /^[A-Za-zÀ-ÖØ-öø-ÿ\s]{10,120}$/,
         user_name:     /^[A-Za-z0-9_-]{1,120}$/,
         user_city:     /^[A-Za-zÀ-ÖØ-öø-ÿ\s-]{2,120}$/,
         user_cpf:      /^[0-9]{11}$/,
@@ -9,18 +9,22 @@ export const valid_updat = (user_json) => {
         user_cords:    /^(-?\d{1,2}(\.\d+)?),\s*(-?\d{1,3}(\.\d+)?)$/,
     };
 
-    const required_fields = Object.keys(patterns);
-    const invalid_fields = [];
-
-    for (const field of required_fields) {
-        const value = user_json[field];
-
-        if (!value || !patterns[field].test(value))
-            invalid_fields.push(field);
+    if (user_json instanceof Object) {
+        const data_key = Object.keys(user_json);
+        const invalids = [];
+        
+        if (data_key.length > 0 ) {
+            for (let item of data_key) {
+                let value = user_json[item];
+                let teste = patterns_[item];
+                if (teste == undefined || teste.test(value) == false)
+                    invalids.push(item);
+            }
+            return {
+                hasErros: invalids.length > 0,
+                invalids
+            }
+        }
     }
-
-    return {
-        has_errors: invalid_fields.length > 0,
-        errors: invalid_fields,
-    };
+    return null;
 };
