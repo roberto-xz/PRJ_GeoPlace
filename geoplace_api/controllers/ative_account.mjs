25d146fb9d733aa9d365a500d1d7f0b0ef7be93c
roberto-xz <devv-robert@outlook.com>
2025-01-15 22:23:46 -0300

    adapttado código para seguir o novo padrão de reconhecimento de tipo do scode no redis, um scode agora deve ter um campo type, para identificar seu propósito

--- geoplace_api/controllers/ative_account.mjs
+++ geoplace_api/controllers/ative_account.mjs
@@ -5,21 +5,26 @@ import {models} from '../models/models.mjs'
 export const active_account = async (req,res) =>{
     let scode = req.body.gp_scode;
     let redis = await rediscnnx();
-
+    
     if (redis == null) {
        console.log('[geoplace_api] Redis is not connected:: /ative');
        return res.status(500).send(); // Internal Server Error
-    } 
-    const timer = await redis.ttl(scode);
-    if (timer > 2 ) {
-        let email = await redis.get(scode);
-        let [count,_] = await models.Users.update(
-            {user_account_status: true},
-            {where: {user_email:email}}
-        );
-        if (count > 0 ) {
-            redis.del(scode);
-            return res.status(200).send() // 200 - Ok
+    }
+    
+    let rdata = await redis.get(scode);
+    rdata = JSON.parse(rdata);
+
+    if (rdata.ctype == 'ative_account') {
+        const timer = await redis.ttl(scode);
+        if (timer > 2 ) {
+            let [count,_] = await models.Users.update(
+                {user_account_status: true},
+                {where: {user_email:rdata.email}}
+            );
+            if (count > 0 ) {
+                redis.del(scode);
+                return res.status(200).send() // 200 - Ok
+            }
         }
     }
     res.status(401).send() // Unauthorized
