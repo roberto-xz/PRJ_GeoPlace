3c559b2108a08dad187803e535fadd10c009868c
roberto-xz <devv-robert@outlook.com>
2025-01-18 19:57:54 -0300

    Novo nome para o arquivo de configuração da API

---
+++ geoplace_spa/api-server.conf.mjs
@@ -0,0 +1,8 @@
+
+
+export default {
+    API_HOST: '127.0.0.1',
+    API_PORT: 3002,
+    API_URL: 'http://localhost:3002'
+}
+
--- geoplace_spa/api.config.mjs
+++
@@ -1,8 +0,0 @@
-
-
-const API_CONFIGS = {
-    API_HOST: '127.0.0.1',
-    API_PORT: 3002,
-}
-
-export default API_CONFIGS;
