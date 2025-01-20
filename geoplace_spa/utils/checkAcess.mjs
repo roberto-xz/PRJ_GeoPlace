1d86ee9279baf1a554e44abad38265c064a721d4
roberto-xz <devv-robert@outlook.com>
2025-01-20 17:09:24 -0300

    Checa por tokens e redireciona o usuário para a rota mais adequada..

---
+++ geoplace_spa/utils/checkAcess.mjs
@@ -0,0 +1,35 @@
+
+import Apicf from '../apiconfig.mjs'
+
+export default async (app_route)=> {
+    let login_token = window.localStorage.getItem('gpl_lgToken');
+    let account_sts = window.localStorage.getItem('gpl_isPendg')
+    
+    if (account_sts) {
+        console.log('há um conta pendente, redirecioando');
+        return app_route.push('/created');
+    }
+    if (login_token != null) {
+        console.log('há um token de sessão, tentando validar');
+        const body = {
+            method: 'POST',
+            headers: { 'content-type': 'application/json'},
+            body: JSON.stringify({
+                user_token: login_token || ''
+            })
+        }
+        try {
+            const resp = await fetch(Apicf.API_URL+'/check-token',body)
+            if (resp.status == 200 ) {
+                console.log('o token é valido, redirecionando')
+                return app_route.push('/home')
+            }
+            console.log('o token é inválido, e foi removido')
+            window.login_token.removeItem('gpl_lgToken')
+            return
+        }
+        catch(e){console.log('A api não respondeu a verificação de token'); return}
+    }
+    console.log('Não há tokens para verificação')
+    return
+}
