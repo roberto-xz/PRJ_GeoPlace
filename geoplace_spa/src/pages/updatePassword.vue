5a66c7f9e17b8a2caaad0e74cecd5ad9fa1f384a
roberto-xz <devv-robert@outlook.com>
2025-01-25 19:27:56 -0300

    Refatorado e adicionado suporte a notificações com o componente Notify

--- geoplace_spa/src/pages/updatePassword.vue
+++ geoplace_spa/src/pages/updatePassword.vue
@@ -5,60 +5,120 @@ import Passw from './component_s/passw.vue'
 
 import { onBeforeMount } from 'vue';
 import Apicf from './../../apiconfig.mjs'
+import Notify from './component_s/notify_popup.vue'
 import checkAcess from '../../utils/checkAcess.mjs';
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const passw_ref = ref(null)
-const app_route = useRouter();
+const show_poup = ref(false)
+const scode = useRoute().params.scode;
+
+
+const popup_attributes = ref({
+    show_loading: true,
+    goto: undefined,
+    button_text: 'Ok, tudo certo',
+    message: 'Opa, tudo joia meu parça',
+    icon: 'warning.png'
+})
 
 onBeforeMount(()=>{ checkAcess(useRouter())})
 
-const send = async ()=>{
-    const diagnostic = document.getElementById('diagnostics')
+const send = async ()=> {
     if (!passw_ref.value.status) return
-
+    show_poup.value = true;
+    popup_attributes.value.show_loading = true;
+    let resp_status = 200;
+    console.log(scode)
     try {
         const body = {
-            method: 'POST',
+            method: 'PUT',
             headers: {'content-type':'application/json'},
             body: JSON.stringify({
-                gp_code: gpcode,
+                gp_code: scode,
                 unpassw: passw_ref.value.passwd
             })
-        }
-
-        res = await fetch(Apicf.API_URL+'/update-pwd',body);
-        switch(res.status){
-            case 404:
-                diagnostic.style.opacity = 1;
-                diagnostic.innerText = 'Usuário Não Encontrado'
-                setTimeout(()=>{
-                    diagnostic.style.opacity = 0;
-                },2000)
-            break;
-            case 200:
-                app_route.push('/login')
-            break;
-        }
-    }catch(e){
-        diagnostic.style.opacity = 1;
-        diagnostic.innerText = 'Servidor Offline'
+        };
+        const res = await fetch(Apicf.API_URL+'/update-pwd',body);
+        resp_status = res.status;
+    }
+    catch(e){
         setTimeout(()=>{
-            diagnostic.style.opacity = 0;
-        }, 2000);
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.message = 'Ué, Nosso servidor não está respondendo, por favor volte outra hora!!'
+        popup_attributes.value.goto = undefined
+        popup_attributes.value.button_text = 'fechar'
+        return
+    }
+
+    if (resp_status == 200 ) {
+        setTimeout(()=>{
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.icon = 'sucsess.png'
+        popup_attributes.value.message = 'Sua senha foi atualizada com sucesso!, Agora você pode fazer login com sua nova senha.'
+        popup_attributes.value.goto = '/login'
+        popup_attributes.value.button_text = 'entrar'
+    }
+    
+    if (resp_status == 400 ) {
+        setTimeout(()=>{
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.message = 'Sua senha não está em um formato válido, tente outra senha'
+        popup_attributes.value.goto = undefined
+        popup_attributes.value.button_text = 'fechar'
+    }
+    
+    if (resp_status == 401 ) {
+        setTimeout(()=>{
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.message = 'Esse link não é valido, Por favor peça um novo link para redefinir sua senha'
+        popup_attributes.value.goto = '/recoverypwd'
+        popup_attributes.value.button_text = 'Pedir novo link'
+    }
+    
+    if (resp_status == 408 ) {
+        setTimeout(()=>{
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.message = 'Link expirado, Por favor peça um novo link para redefinir sua senha'
+        popup_attributes.value.goto = '/recoverypwd'
+        popup_attributes.value.button_text = 'Pedir novo link'
+    }
+    
+    if (resp_status == 412 ) {
+        setTimeout(()=>{
+            popup_attributes.value.show_loading = false;
+        },200);
+        popup_attributes.value.message = 'Por algum motivo desconhecido, sua senha não foi atualizada, tente novamente'
+        popup_attributes.value.goto = undefined
+        popup_attributes.value.button_text = 'fechar'
     }
 }
-
-</script>
+ </script>
     
 <template>
     <div id='updPwd-page'>
-    <main>
-        <img src="/res/geoplace_icon.png" alt="geoplace image logo"/>
-        <div id='diagnostics'>Messagens aki</div>
-        <Passw ref="passw_ref"/>
-        <button @click='send()'>Atualizar</button>
-    </main>
+        <Notify
+            :attributes=popup_attributes
+            v-if='show_poup'
+            @close-popup='()=>{show_poup = !show_poup}'
+        />
+        <main>
+            <img src="/res/geoplace_icon.png" alt="geoplace image logo"/>
+            <header>
+                <h1>Crie sua nova senha</h1>
+                <h5>
+                Escolha uma nova senha para sua conta. Certifique-se de que 
+                ela tenha pelo menos 8 caracteres, incluindo números, letras e simbolos.
+                </h5>
+            </header>
+            <Passw ref="passw_ref"/>
+            <button @click='send()'>Atualizar</button>
+        </main>
     </div>
 </template>
 
@@ -69,9 +129,11 @@ const send = async ()=>{
     justify-content: center;
     align-items: center;
     width: 100vw;
-    height: 70vh;
+    height: 100vh;
 }
-main {width: 70%;}
+main {
+    width: min(430px,85%);
+}
 img {
     display: block;
     width: 110px;
@@ -79,17 +141,24 @@ img {
     margin-bottom: 10px;
     align-self: center;
 }
-#diagnostics {
+
+header {
     display: block;
-    transition: .6s;
     width: 100%;
-    opacity: 0;
-    text-align: center;
-    background-color: rgb(236, 127, 127);
-    font: bolder .8rem/1 "Manjari";
-    color: #f3f3f3;
-    padding: 3%;
+    margin: 20px 0;
 }
+    header h1 {
+        width: 100%;
+        text-align: center;
+        font: bolder 1.5rem/1 "Manjari";
+    }
+    header h5 {
+        width: 100%;
+        text-align: center;
+        font: normal 1rem/1 "Manjari";
+        margin-top: 2%;
+        margin-bottom: 10%;
+    }
 
 button {
     display: block;
