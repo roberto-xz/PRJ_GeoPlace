a2959aeb225f53e9f8b304776becd07316f2bc7f
roberto-xz <devv-robert@outlook.com>
2025-01-25 19:31:37 -0300

    corrigidos códigos de retorno, e adicionado suporte a nova versão da criação de secret codes (scodes)

--- geoplace_api/controllers/update_password.mjs
+++ geoplace_api/controllers/update_password.mjs
@@ -1,13 +1,16 @@
 
 import { models } from "../models/models.mjs";
+import { check_scode } from "../utils/scode.mjs";
 import rediscnnx from "../src/rediscnnx.mjs";
+import validator from 'validator'
 
 export const update_pwd = async (req,res)=> {
     const scode = req.body.gp_code;
     const passw = req.body.unpassw || " ";
-    
-    if (!scode) return res.sendStatus(401);
-    if (!validator.isSha1(passw)) return res.sendStatus(400);
+    let user_data;
+
+    if (!check_scode(scode)) return res.sendStatus(401);
+    if (!validator.isHash(passw,'sha1')) return res.sendStatus(400);
     
     const redis = await rediscnnx();    
     if (redis == null) {
@@ -17,18 +20,20 @@ export const update_pwd = async (req,res)=> {
 
     const timer = await redis.ttl(scode)
     if ( timer > 2 ) {
-        const user_data = await redis.get(scode)
+        user_data = await redis.get(scode)
         user_data = JSON.parse(user_data)
-        if (user_data.ctype != 'recovery_password') return res.sendStatus(400);
+        if (user_data.ctype != 'recovery_password') return res.sendStatus(401);
         
         let [count,_] = await models.Users.update(
-            {where: {user_pass:passw}}
+            {user_pass:passw},
+            {where: {user_email:user_data.email}},
         );
 
-        if (count > 0 ) {
+        if ( count > 0 ) {
             redis.del(scode);
             return res.status(200).send() // 200 - Ok
         }
+        return res.sendStatus(412)
     }
-    return res.sendStatus(401)
+    return res.sendStatus(408)
 }
