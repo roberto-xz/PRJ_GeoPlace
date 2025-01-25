c3f86bf71d28fe8b85937d68526cc56d763cafab
roberto-xz <devv-robert@outlook.com>
2025-01-25 19:22:59 -0300

    Modificado módulo scode, melhorado a criação e adicionado uma função para validação do código gerado

---
+++ geoplace_api/utils/scode.mjs
@@ -0,0 +1,21 @@
+
+import crypto from 'crypto'
+
+export const gen_scode = ()=> {
+    const dict = "abcdEfghijkLmnop012345QRSTUVXYZ_"
+    let scode = 'gEpL_'
+    
+    for (let i = 0; i<20; i++) {
+        let rnd = crypto.randomInt(0,dict.length)
+        scode += dict[rnd];
+    }
+    return scode;
+}
+
+export const check_scode = (scode)=> {
+    if (scode == null || scode == undefined )
+        return false;
+    
+    const dict = "abcdEfghijkLmnop012345QRSTUVXYZ_"
+    return [...scode].every(char => dict.includes(char))
+}
