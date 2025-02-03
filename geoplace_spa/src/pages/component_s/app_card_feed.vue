eee7adb6d24e03dee213d3e2485b2f12038c474c
roberto-xz <devv-robert@outlook.com>
2025-02-03 16:11:01 -0300

    Melhorado o visual do feed de produtos

--- geoplace_spa/src/pages/component_s/app_card_feed.vue
+++ geoplace_spa/src/pages/component_s/app_card_feed.vue
@@ -4,70 +4,109 @@
 
 <template>
 <div id="cards-box">
-    <div class="card" v-for="x in 20" v-bind:key='x' @click="$emit('open-card',x)">
-        <h1>Título do anuncio, um pouco grande {{x}}</h1>
-        <img src='/res/noImageAvailiable.webp' alt=""/>
-        <ul>
-            <li><img src="/res/filter.svg" alt="con-ico"> Venda/Aluguel</li>
-            <li><img src="/res/filter.svg" alt="con-ico"> 3 banheiros</li>
-            <li><img src="/res/filter.svg" alt="con-ico"> 2 cozinhas</li>
-        </ul>
-        <p> preço: 178.000, 00 R$
-            <span>Cabrobó-PE</span>
-        </p>
-    </div>
+    <main>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Mansão muito Xique, só pra quem pode, Se é pobre nem olhe </h1>
+            <img src='/res/test_images/01.jpg' alt=""/>
+            <p> preço: 1.578.000,00 R$
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Casa 3d, Feita no blender, Comprou a casa, leva o carro </h1>
+            <img src='/res/test_images/02.jpg' alt=""/>
+            <p> preço: 15,00 R$
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Casa muito boa para Físicos, A sacada mais baixa tem 7 metros </h1>
+            <img src='/res/test_images/03.jpg' alt=""/>
+            <p> preço: 3.000,00 R$/Més
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Só vendo, Não troco, Dispenso curiosos, Salmos 23 </h1>
+            <img src='/res/test_images/04.jpg' alt=""/>
+            <p> preço: 308.000,00 R$
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Tá em ótimo estado, o último morador, nem queria sair da casa </h1>
+            <img src='/res/test_images/05.jpg' alt=""/>
+            <p> preço: 230,00 R$/Més
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+        <div class="card" @click="$emit('open-card',12)">
+            <h1>Casa com piscina, ótima opção para solteiros </h1>
+            <img src='/res/test_images/06.jpg' alt=""/>
+            <p> preço: 120, 00 R$/ Més
+                <span>Cabrobó-PE</span>
+            </p>
+        </div>
+    </main>
 </div>
 </template>
 
 <style scoped>
-    #cards-box {
-        display: block;
-        width: 94%;
-        height: 80vh;
-        overflow: auto;
-        margin: 0 auto;
-        padding: 5px 0;
-    }
-    .card {
-        overflow: hidden;
-        width: 450px;
-        height: 422px;
-        margin: auto auto;
-        margin-bottom: 40px;    
-    }
-        h1 {
-            width: 100%;
-            font: bolder 1rem/1 "Manjari";
-            padding: 10px 0;
-        }
+#cards-box {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    height: 90vh;
+    margin: 0 auto;
+    padding: 5px 0;
+}
 
-        img {
-            width: 100%;
-            height: 75%;
-        }
-        ul {
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-            align-content: center;
-            list-style: none;
-            font: bolder 1rem/1 "Manjari";
-        }
-            ul li img {
-                vertical-align: middle;
-                width: 32px;
-                height: auto;
-            }
-        p {
-            display: block;
-            width: 100%;
-            font: bolder 1rem/1 "Manjari";
-            margin: 10px 0px;
-            padding-bottom: 3px;
-            border-bottom: 1px solid #000;
-        }
+main {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    width: min(1300px, 95%);
+    height: 100%;
+    overflow: auto;
+    scrollbar-color: darkgreen #f4f4f4;
+    scrollbar-width: thin;
+}
 
-        p span {
-            float: right;
-        }
+.card {
+    display: block;
+    margin: 30px 0;
+    width: 370px;
+    height: 370px;
+}
+
+h1 {
+    width: 100%;
+    font: 500 1.2rem/1 "Manjari";
+    padding: 10px 0;
+}
+
+img {
+    cursor: pointer;
+    width: 100%;
+    height: 75%;
+    transition: .7s;
+    border: 1px solid #000;
+}
+    img:hover {
+        transform: scale(1.05);
+        transition: .7s;
+    }
+
+
+p {
+    display: block;
+    width: 100%;
+    font: bolder 1rem/1 "Manjari";
+    margin: 10px 0px;
+    padding-bottom: 3px;
+    border-bottom: 1px solid #000;
+}
+
+p span {float: right;}
+
 </style>
