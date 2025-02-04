2360f66266ef7eb82f2f69eae6d7e1f825a0f026
roberto-xz <devv-robert@outlook.com>
2025-02-04 15:38:29 -0300

    adicionado componente para criação de produtos

---
+++ geoplace_spa/src/pages/createProduct.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div id='create-product-page'>
+    <main>
+        <div id='screen'>
+            <div id='l-screen images-form'>
+                <h1>Adicione boas fotos</h1>
+                <label>Adicionar foto</label>
+                <div id='collum-pictures'>
+                    <label class='collum-image'>Adicionar foto</label>
+                    <label class='collum-image'>Adicionar foto</label>
+                    <label class='collum-image'>Adicionar foto</label>
+                    <label class='collum-image'>Adicionar foto</label>
+                    <label class='collum-image'>Adicionar foto</label>
+                    <label class='collum-image'>Adicionar foto</label>
+                </div>
+            </div>
+            
+            <div id='r-screen inputs-form'></div>
+        </div>
+    </main>
+</div>
+</template>
